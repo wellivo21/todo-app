afe9023fafdf803b4468d27b4747196690651586
Wellington Ivo <wellingtonivoprog@gmail.com>
2022-10-01 12:16:25 -0300

    add modify task functionality. Fix minor bugs

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,7 @@ const titleErrorMsgEl = document.querySelector('.title-error-msg');
 const dateErrorMsgEl = document.querySelector('.date-error-msg');
 const textErrorMsgEl = document.querySelector('.text-error-msg');
 const tasksSectionEl = document.querySelector('.tasks-section');
+const modalEl = document.querySelector('#taskModal');
 
 // event listeners
 taskFormEl.addEventListener('submit', (event) => {
@@ -41,11 +42,17 @@ const formValidation = () => {
       text: '',
     };
     data.title = titleData;
-    data.date = dateData;
     data.text = textareaData;
+
+    // separating date to format in day/month/year
+    const dateParts = dateData.split('-');
+    const [year, month, day] = dateParts;
+    data.date = `${day}-${month}-${year}`;
 
     return data;
   }
+
+  return false;
 };
 
 const createTask = () => {
@@ -59,24 +66,63 @@ const createTask = () => {
     <p class="task-text">
       ${text}
     </p>
-    <i class="icon modify-task-btn fa-solid fa-pen-to-square"></i>
+    <i class="icon modify-task-btn fa-solid fa-pen-to-square" data-toggle="modal"
+    data-target="#taskModal"></i>
     <i class="icon delete-task-btn fa-solid fa-trash-can"></i>
   </article>
   `;
   tasksSectionEl.insertAdjacentHTML('beforeend', markup);
+
+  // hiding modal
+  $('.modal').modal('hide');
+
+  // reseting inputs
+  modalTitleInputEl.value = '';
+  modalDateInputEl.value = '';
+  modalTextareaEl.value = '';
+};
+
+const modifyTask = () => {
+  tasksSectionEl.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modify-task-btn')) {
+      const clickedTask = event.target.closest('.task');
+
+      const currentTaskTitle =
+        clickedTask.querySelector('.task-title').innerHTML;
+      const currentTaskDate = clickedTask.querySelector('.task-date').innerHTML;
+      const currentTaskText = clickedTask.querySelector('.task-text').innerHTML;
+
+      modalTitleInputEl.value = currentTaskTitle;
+      modalTextareaEl.value = currentTaskText.trim();
+
+      // code for preseting date on date input field
+      const currentTaskDateParts = currentTaskDate.split('-');
+      const [day, month, year] = currentTaskDateParts;
+      const currentDateObj = new Date(year, month - 1, day);
+      modalDateInputEl.value = currentDateObj.toISOString().substring(0, 10);
+
+      // when submit button is clicked, remove task and replace it by the new one
+      if (formValidation) {
+        submitTaskBtnEl.addEventListener('click', () => {
+          clickedTask.remove();
+        });
+      }
+    }
+  });
 };
 
 const deleteTask = () => {
   tasksSectionEl.addEventListener('click', (event) => {
     if (event.target.classList.contains('delete-task-btn')) {
-      const task = document.querySelector('.task');
-      task.remove();
+      const currentTask = event.target.closest('.task');
+      currentTask.remove();
     }
   });
 };
 
 const init = () => {
   deleteTask();
+  modifyTask();
 };
 
 init();
